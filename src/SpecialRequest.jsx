86339450755d5d055553b8499805e7bca646f19a
Jamie Bryson <jamieab98@gmail.com>
2025-11-15 09:44:29 -0500

    Made it so when the form is submitted, it takes all of the user input and stores it in a variable called newJob

--- src/SpecialRequest.jsx
+++ src/SpecialRequest.jsx
@@ -1,8 +1,22 @@
 import NavigationBar from "./NavigationBar"
+import { useState, useId } from "react";
+
 function Request() {
+    const [newJobName, setNewJobName] = useState("")
+    const [newDescription, setNewDescription] = useState("")
+    const [newPrice, setNewPrice] = useState(0)
+    const newId = useId();
+
     function handleRequest(e){
         e.preventDefault();
-        console.log("Form submitted")
+        const newJob = {
+            "id": newId,
+            "job": newJobName,
+            "description": newDescription,
+            "hourlyrate": newPrice,
+            "customerrating": "0 Stars"
+        }
+        console.log(newJob)
     }
 
     return(
@@ -11,11 +25,11 @@ function Request() {
             <h3>Create New Service</h3>
             <form onSubmit = {handleRequest}>
                 <label>Job: </label>
-                <input type="text" placeholder="Name of Job"/>
+                <input type="text" value={newJobName} onChange={(e)=>setNewJobName(e.target.value)} placeholder="Name of Job"/>
                 <label>Decription: </label>
-                <input type="text" placeholder="Description"/>
+                <input type="text" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} placeholder="Description"/>
                 <label>Hourly Rate Suggestion:</label>
-                <input type="number"></input>
+                <input type="number" value={newPrice} onChange={(e)=>setNewPrice(e.target.value)}></input>
                 <button type="submit">Request Job</button>
             </form>
         </>

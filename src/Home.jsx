73719fe372339f1ac90ebdbe6a7736dc8299d2c6
Jamie Bryson <jamieab98@gmail.com>
2025-11-15 07:58:20 -0500

    Added some text to the home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,18 @@
 import NavigationBar from "./NavigationBar"
-
 function Home() {
 
     return(
         <>
             <NavigationBar />
-            <div>Home Components</div>
+            <div>
+                <h2>My Service Shop!</h2>
+            </div>
+            <div>
+                <p>Please navigate through the page to see what service you might need.</p>
+            </div>
+            <div>
+                <p>If you don't see the service that you're looking for, feel free to put in a special request through the request tab!</p>
+            </div>
         </>
     )
 }

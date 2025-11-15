56f7f0f9fee3a0be3baa44edcc769cd2d4f8a3f2
Jamie Bryson <jamieab98@gmail.com>
2025-11-15 09:08:50 -0500

    Set up the form and with the submit button

--- src/SpecialRequest.jsx
+++ src/SpecialRequest.jsx
@@ -1,10 +1,23 @@
 import NavigationBar from "./NavigationBar"
 function Request() {
+    function handleRequest(e){
+        e.preventDefault();
+        console.log("Form submitted")
+    }
 
     return(
         <>
             <NavigationBar />
-            <div>Special Request Component</div>
+            <h3>Create New Service</h3>
+            <form onSubmit = {handleRequest}>
+                <label>Job: </label>
+                <input type="text" placeholder="Name of Job"/>
+                <label>Decription: </label>
+                <input type="text" placeholder="Description"/>
+                <label>Hourly Rate Suggestion:</label>
+                <input type="number"></input>
+                <button type="submit">Request Job</button>
+            </form>
         </>
     )
 }

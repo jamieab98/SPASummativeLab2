aa16798dafc584634e7f5f55f96f6699ca9f5a5c
Jamie Bryson <jamieab98@gmail.com>
2025-11-15 08:29:54 -0500

    Made it so Store will fetch the jobs from the local server and set the state. Then mapped each service to create a new ServiceCard

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,6 +1,20 @@
 import NavigationBar from "./NavigationBar"
+import { useEffect, useState } from "react"
+import ServicesCard from "./ServicesCard"
+const jobsurl = "http://localhost:3001/jobs"
+
 function Store() {
-    
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch(jobsurl)
+        .then(response => response.json())
+        .then((data) => {
+            setServices(data)
+            console.log(data)
+        })
+        .catch(error => console.log(error))
+    }, [])
 
     return(
         <>
@@ -9,7 +23,9 @@ function Store() {
                 <h2>List of Services</h2>
             </div>
             <div>
-
+                {services.map((service) => (
+                    <ServicesCard  />
+                ))}
             </div>
         </>
     )

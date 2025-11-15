afb7754154591bfc52632acc76f604bf4d58f4fa
Jamie Bryson <jamieab98@gmail.com>
2025-11-15 08:45:01 -0500

    Fixed some typos

--- src/ServiceCard.jsx
+++ src/ServiceCard.jsx
@@ -4,8 +4,9 @@ function ServiceCard({service}) {
         <>
             <h3>Service: {service.job}</h3>
             <div>Description: {service.description}</div>
-            <div>Hourly Rate: {service.horlyrate}</div>
+            <div>Hourly Rate: ${service.hourlyrate}</div>
             <div>Customer Rating: {service.customerrating}</div>
+            <button>Book Service</button>
         </>
     )
 }

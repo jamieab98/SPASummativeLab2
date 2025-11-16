bfdea11d80d5bd8df8d71895319f640d1bcdcf40
Jamie Bryson <jamieab98@gmail.com>
2025-11-16 17:56:01 -0500

    Made the search input

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,12 +1,15 @@
 import NavigationBar from "./NavigationBar"
-import { useEffect, useContext, } from "react"
+import { useEffect, useContext,useState } from "react"
 import ServiceCard from "./ServiceCard"
 import ServicesContext from "./ServicesContext"
 const jobsurl = "http://localhost:3001/jobs"
 
 function Store() {
     const {services, setServices} = useContext(ServicesContext)
-
+    const [searchedService, setSearchedService] = useState("")
+    const filteredServices = services.filter((service) => 
+        service.job.toLowerCase().includes(searchedService.toLowerCase())
+    )
 
     useEffect(() => {
         fetch(jobsurl)
@@ -22,10 +25,10 @@ function Store() {
             <NavigationBar />
             <div>
                 <h2>List of Services</h2>
-                <input type="text" placeholder="Seach..."></input>
+                <input type="text" placeholder="Seach..." value={searchedService} onChange={(e)=>setSearchedService(e.target.value)} id="userSearch"></input>
             </div>
             <div>
-                {services.map((service) => (
+                {filteredServices.map((service) => (
                     <div key={service.id}>
                         <ServiceCard service={service}/>
                     </div>

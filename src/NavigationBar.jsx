c804057e0e0fbfcb34c713faa896d58dd585ff84
Jamie Bryson <jamieab98@gmail.com>
2025-11-18 15:49:17 -0500

    Styled the navigation bar

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
+import styles from "../src/cssmodules/Navigation.module.css"
 function NavigationBar() {
 
     return(
         <>
-            <div data-testid="NavigationBarDiv">
-                <Link to="/">Home</Link>  |{" "}
-                <Link to="/store">Store</Link>  |{" "}
-                <Link to="/specialrequest">Special Request</Link>  |{" "}
-                <Link to="/admin">Admin Portal</Link>
+            <div data-testid="NavigationBarDiv" className={styles.navigationbartabs}>
+                <Link to="/" className={styles.navigationbartab}>Home</Link>
+                <Link to="/store" className={styles.navigationbartab}>Store</Link>
+                <Link to="/specialrequest" className={styles.navigationbartab}>Special Request</Link>
+                <Link to="/admin" className={styles.navigationbartab}>Admin Portal</Link>
             </div>
         </>
     )

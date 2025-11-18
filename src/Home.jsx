c11ab91ce0c5493eab21bd49b0aa278b7eda031c
Jamie Bryson <jamieab98@gmail.com>
2025-11-18 05:04:54 -0500

    Created module folder. Also started styling the home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import NavigationBar from "./NavigationBar"
+import styles from "../src/cssmodules/Home.module.css"
 function Home() {
 
     return(
         <>
             <NavigationBar />
-            <div>
+            <div className={styles.homecontent}>
+            <div className={styles.homeheading}>
                 <h2>My Service Shop!</h2>
             </div>
             <div>
@@ -12,6 +14,7 @@ function Home() {
             </div>
             <div>
                 <p>If you don't see the service that you're looking for, feel free to put in a special request through the special request tab!</p>
+            </div>
             </div>
         </>
     )

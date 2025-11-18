6fb8a813f8098e482974333846632867b941d697
Jamie Bryson <jamieab98@gmail.com>
2025-11-18 17:15:50 -0500

    Styled Store

--- src/Store.jsx
+++ src/Store.jsx
@@ -3,6 +3,7 @@ import { useContext, useState } from "react"
 import ServiceCard from "./ServiceCard"
 import ServicesContext from "./ServicesContext"
 import useFetchJobs from "./useFetchJobs"
+import styles from "../src/cssmodules/Store.module.css"
 const jobsurl = "http://localhost:3001/jobs"
 
 function Store() {
@@ -17,16 +18,18 @@ function Store() {
     return(
         <>
             <NavigationBar />
-            <div>
-                <h2>List of Services</h2>
-                <input type="text" placeholder="Seach..." value={searchedService} onChange={(e)=>setSearchedService(e.target.value)} id="userSearch" data-testid="ServiceSearchBar"></input>
+            <div className={styles.store}>
+            <div className={styles.servicesearchbar}>
+                <div className={styles.searchheading}>List of Services</div>
+                <input type="text" placeholder="Seach..." value={searchedService} onChange={(e)=>setSearchedService(e.target.value)} id="userSearch" data-testid="ServiceSearchBar" className={styles.searchinput}></input>
             </div>
-            <div>
+            <div className={styles.servicescontainer}>
                 {filteredServices.map((service) => (
-                    <div key={service.id}>
+                    <div key={service.id} className={styles.servicecard}>
                         <ServiceCard service={service}/>
                     </div>
                 ))}
+            </div>
             </div>
         </>
     )
